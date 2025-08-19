b332ef5502d237f9ee8b0bc357ad29a077345f2a
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-19 21:29:00 +0430

    Add show_book funtion.

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def add_book(title, author, year):
     book = {"title": title, "author": author, "year": year}
     books.append(book)
     print(f"Book '{title}' added successfully.")
+
+
+def show_books():
+    """Display all books"""
+    if not books:
+        print(" No books available.")
+    else:
+        for i, book in enumerate(books, start=1):
+            print(f"{i}. {book['title']} - {book['author']} ({book['year']})")
